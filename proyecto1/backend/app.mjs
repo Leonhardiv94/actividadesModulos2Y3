6b8b05d36deaa27f1140086f8dff124f1619cf93
Leonhard <leonhardiv94@gmail.com>
2024-07-02 04:41:06 -0500

    push

--- proyecto1/backend/app.mjs
+++ proyecto1/backend/app.mjs
@@ -1,10 +1,10 @@
 //1. Importamos las dependecias y modulos que necesitamos.
 import express from "express";
 import dotenv from "dotenv";
-//Variables de entorno -> nos gusrdan informacion delicada
+//Variables de entorno -> nos guardan informacion delicada
 import connectionMongo from "./config/db.mjs";
 
-//2. Comfigurrar el uso de nuestro servidor y nuestras variables de:
+//2. Comfigurar el uso de nuestro servidor y nuestras variables de:
 const app = express(); //configuramos nuestro servidor
 dotenv.config(); //configuramos nuestras variables de entorno
 const port = process.env.PORT;
