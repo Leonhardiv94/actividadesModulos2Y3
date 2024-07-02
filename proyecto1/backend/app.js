f472f72bb8a05f2d356c146b29561ed5e77d5357
Leonhard <leonhardiv94@gmail.com>
2024-07-01 23:40:16 -0500

    actualizacion

--- proyecto1/backend/app.js
+++ proyecto1/backend/app.js
@@ -1,1 +1,14 @@
-console.log("hola, estoy existiendo");
+//1. Importamos las dependecias y modulos que necesitamos.
+import express from "express";
+import dotenv from "dotenv";
+//Variables de entorno -> nos gusrdan informacion delicada
+
+//2. Comfigurrar el uso de nuestro servidor y nuestras variables de:
+const app = express(); //configuramos nuestro servidor
+const port = process.env.PORT;
+dotenv. config(); //configuramos nuestras variables de entorno
+
+//3. Escuchar nuestro servidor.
+app.listen(port, ()=>{
+    console.log("Estoy funcionando");
+});
